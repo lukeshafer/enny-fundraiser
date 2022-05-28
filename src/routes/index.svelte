7b0e8c96ea3286442e25040908ba730bbfecd02f
Luke Shafer <hello@lukeshafer.com>
2022-05-27 22:25:59 -0400

    Added Zine, moved all styles into css file

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -26,11 +26,11 @@
 
 	let y: number;
 	let windowHeight: number;
-	let footer: HTMLElement;
+	let linkSection: HTMLElement;
 	let footerTop = 0;
 
 	onMount(() => {
-		if (footer) footerTop = footer.offsetTop;
+		if (linkSection) footerTop = linkSection.offsetTop;
 	});
 
 	const scrollToTop = () => {
@@ -134,27 +134,30 @@
 			terrains
 		</strong>.
 	</p>
-
+	<h2>How you can support!</h2>
 	<p>
-		Click the <strong>Donate</strong> button below to visit the Chuffed.org campaign for Enny's
-		wheelchair. Alternatively, if you prefer to use Paypal or Venmo, follow the links below to
-		donate!
+		Click the <strong>Donate</strong> button below to visit the Chuffed.org campaign and donate
+		directly! Alternatively, if you prefer to use Paypal or Venmo, follow the links below to donate!
 		<em
 			>If you pay with Venmo, please make sure to leave a note mentioning the donation is for Enny!</em
 		>
 	</p>
 </main>
 
-<footer bind:this={footer}>
+<section id="link-section" bind:this={linkSection}>
 	<div class="btn-container btn-footer">
-		<a class="btn" href="https://chuffed.org/project/ennys-wheels">Donate!</a>
+		<a class="btn" href="https://chuffed.org/project/ennys-wheels">Donate directly!</a>
 	</div>
+	<a class="zine" href="https://www.anahitacreates.com/product/fundraiser-better-place-zine"
+		><p>Purchase a <em>Better Place Zine</em> from AnahitaCreates!</p>
+	</a>
 	<div class="link-container">
 		<a
 			id="venmo"
 			class="alt-payment-link"
 			href="https://account.venmo.com/pay?recipients=anahitacreates&note=For%20Enny!"
 			title="AnahitaCreates Venmo"
+			target="_blank"
 			><img src="/assets/venmo_logo_blue.svg" width="240" alt="Donate with Venmo" /></a
 		>
 		<a
@@ -165,232 +168,6 @@
 			><img src="/assets/pp-logo-200px.png" width="240" alt="Donate with PayPal" /></a
 		>
 	</div>
-</footer>
+</section>
 
-<style>
-	header {
-		visibility: hidden;
-	}
-
-	header.activate {
-		visibility: visible;
-	}
-
-	#background-layout,
-	#background-layout * {
-		z-index: -1;
-	}
-
-	.header-wrapper {
-		height: 100vh;
-		display: flex;
-		align-items: center;
-	}
-
-	#background-layout {
-		background: url('/assets/background.png');
-		background-size: cover;
-		position: fixed;
-		width: 100%;
-		height: 100%;
-	}
-
-	.chair {
-		width: 6em;
-		position: absolute;
-	}
-
-	#burst-wrapper {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		height: 100%;
-	}
-
-	#burst {
-		height: 90vh;
-	}
-
-	#enny {
-		height: calc(100% + 2em);
-		width: calc(100% + 2em);
-		grid-row: 2;
-		grid-column: 2 / 3;
-		object-fit: contain;
-	}
-
-	.cloud {
-		position: absolute;
-		object-fit: contain;
-		width: 50em;
-	}
-
-	.cloud-container {
-		position: relative;
-		grid-column: 2;
-		grid-row: 2;
-	}
-
-	#cloud-left {
-		bottom: -10em;
-		right: -2em;
-	}
-
-	#cloud-right {
-		bottom: -10em;
-		left: -2em;
-	}
-
-	strong {
-		--outline: var(--strong);
-		font-weight: 900;
-		color: var(--dark-second);
-		text-shadow: 0 0 5px var(--outline);
-		letter-spacing: 1px;
-	}
-
-	a {
-		text-decoration: none;
-	}
-
-	.btn-container {
-		/* grid-column: 1/3; */
-		z-index: 1;
-		/* justify-self: left; */
-		display: flex;
-		flex-flow: column;
-		align-items: center;
-	}
-
-	.btn::before {
-		align-self: center;
-		background: #00000000 url('/assets/hand-heart.svg') no-repeat center;
-		background-size: contain;
-		display: block;
-		width: 100%;
-		height: 2em;
-		content: '';
-	}
-
-	.btn {
-		color: var(--btn-text);
-		background: #00000077;
-		padding: 0.3em;
-		font: var(--subheading);
-		font-size: 3em;
-		white-space: nowrap;
-	}
-
-	.btn:hover {
-		font-size: 3.2em;
-		filter: brightness(120%);
-	}
-
-	.float-btn {
-		position: fixed;
-		bottom: 1em;
-		right: 1em;
-	}
-
-	.float-btn:hover {
-		font-size: 3.7em;
-		filter: brightness(120%);
-	}
-
-	.link-container {
-		display: grid;
-		width: 100%;
-		grid-template-columns: repeat(auto-fit, minmax(12em, 1fr));
-		grid-auto-rows: 4em;
-		place-items: center;
-		gap: 2em;
-		/* justify-content: center;
-		flex-flow: row wrap; */
-	}
-
-	.alt-payment-link {
-		display: block;
-		height: 2em;
-		width: auto;
-		background: white;
-		padding: 1em;
-	}
-
-	.alt-payment-link:hover {
-		padding: 1.2em;
-		filter: brightness(120%);
-	}
-
-	.alt-payment-link > img {
-		height: 2em;
-		width: 8em;
-		object-fit: cover;
-	}
-
-	h1 {
-		opacity: var(--opacity);
-	}
-
-	h1.scrollToTop:hover {
-		cursor: pointer;
-		opacity: 1;
-	}
-
-	@media (prefers-reduced-motion: reduce) {
-		header.activate > h1 {
-			animation: none;
-		}
-		header.activate img#cloud-left {
-			animation: none;
-		}
-
-		header.activate img#cloud-right {
-			animation: none;
-		}
-
-		.btn {
-			transition: none;
-		}
-
-		#venmo,
-		#paypal {
-			transition: none;
-		}
-	}
-
-	@media (prefers-reduced-motion: no-preference) {
-		header.activate > h1 {
-			animation: fade-in 800ms ease-out;
-		}
-
-		h1 {
-			transition: opacity 300ms;
-		}
-
-		header.activate img.cloud {
-			--delay: 500ms;
-			transition: visibility 800ms var(--delay);
-		}
-		header.activate img#cloud-left {
-			animation: slide-in-left 800ms ease-out var(--delay);
-		}
-
-		header.activate img#cloud-right {
-			animation: slide-in-right 800ms ease-out var(--delay);
-		}
-
-		header.activate img#enny {
-			transition: visibility 800ms 601ms;
-			animation: slide-in-bottom 800ms ease-out 600ms;
-		}
-
-		.btn {
-			transition: font-size 200ms cubic-bezier(0.57, -1, 0.41, 2);
-		}
-
-		#venmo,
-		#paypal {
-			transition: padding 200ms cubic-bezier(0.57, -1, 0.41, 2);
-		}
-	}
-</style>
+<footer><h2>Thank you for your support! ♥️</h2></footer>
